11b04242b1ffb1136523f5dc25a3de69c8cefc8f
Wicchi <74045262+Wicchi@users.noreply.github.com>
2021-10-08 11:11:43 +0200

    Add files via upload

--- sem_1.R
+++ sem_1.R
@@ -6,6 +6,8 @@ dta <- read.fwf(file = "https://hydrology.nws.noaa.gov/pub/gcip/mopex/US_Data/Us
 head(x = dta)
 str(object = dta)
 
+print ("Lol hi, it is works")
+
 dta$DTM <- as.Date(x = gsub(pattern = " ",
                             replacement = "0",
                             x = dta$DTM),
@@ -22,55 +24,55 @@ ids <- gsub(pattern = ".dly",
             x = ids)
 head(ids)
 
-data_download <- function(id, 
-                          save = FALSE, 
-                          path = NULL, 
+data_download <- function(id,
+                          save = FALSE,
+                          path = NULL,
                           file_name = NULL) {
-  
+
   dta <- lapply(
-    X = id, 
+    X = id,
     FUN = function(x) {
-      
+
       message(paste("downloading", x))
-      
-      dl <- read.fwf(file = paste0("https://hydrology.nws.noaa.gov/pub/gcip/mopex/US_Data/Us_438_Daily/", 
+
+      dl <- read.fwf(file = paste0("https://hydrology.nws.noaa.gov/pub/gcip/mopex/US_Data/Us_438_Daily/",
                                    x,
                                    ".dly"),
                      widths = c(8, rep(x = 10,
                                        times = 5)),
                      col.names = c("DTM", "P", "E", "Q", "Tmax", "Tmin"))
-      
+
       dl$DTM <- as.Date(x = gsub(pattern = " ",
                                  replacement = "0",
                                  x = dl$DTM),
                         format = "%Y%m%d")
       dl$ID <- x
-      
+
       dl
-    } 
+    }
   )
-  
+
   dta_bind <- do.call(what = rbind,
                       args = dta)
   dta_bind$ID <- as.factor(x = dta_bind$ID)
 
   if (save) {
-    
-    if (any(is.null(x = path), 
+
+    if (any(is.null(x = path),
             is.null(x = file_name))) {
-      
+
       stop("path or filename was not supplied")
     }
-    
+
     write.csv(x = dta_bind,
               file = file.path(path, file_name))
-    
+
     if (file.exists(file.path(path, file_name))) {
-      
+
       message("file created succesfully")
     }
   }
-  
+
   dta_bind
 }
 
@@ -78,9 +80,9 @@ test <- data_download(id = ids[5:10])
 
 str(test)
 
-test <- data_download(id = ids[1], 
+test <- data_download(id = ids[1],
                       save = TRUE)
-test <- data_download(id = ids[1], 
+test <- data_download(id = ids[1],
                       save = TRUE,
                       path = getwd(),
                       file_name = "data.csv")
